841ae652931684e8dee476cb47b284f32728651b
第一次提交 <739988982@qq.com>
2023-06-07 21:54:33 +0800

    分支提交11

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print('bbb')
     print('ccc')
-    print('分之')
+    print('分之1')
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
